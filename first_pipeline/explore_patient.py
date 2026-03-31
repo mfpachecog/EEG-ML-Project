65379a0a7b9aa13ae4d2617a7139bbd4a6b7f165
mfpachecog <manuelfpg012@gmail.com>
2026-03-31 13:38:04 -0500

    feat:create constants for data directory, and patient id

--- first_pipeline/explore_patient.py
+++ first_pipeline/explore_patient.py
@@ -16,5 +16,9 @@ import wfdb
 import numpy as np
 import matplotlib.pyplot as plt
 
+#route to the file where the patients data is downloaded. 
+DATA_DIR = "/home/singular1ty/Documents/_PROJECTS/eeg-ml-project/patients_data_raw/physionet.org/files/i-care/2.1/training"
 
+#patient ID from the downloaded patients
+PATIENT_ID = "0342"
 
