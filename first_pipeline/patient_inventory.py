682611b45f6a02227190f36baafcc40882576554
mfpachecog <manuelfpg012@gmail.com>
2026-04-17 09:47:51 -0500

    feat:create first comment for script explanation and import libraries

---
+++ first_pipeline/patient_inventory.py
@@ -0,0 +1,24 @@
+
+"""
+PHASE 1: INVENTORY 
+
+OBJECTIVE: 
+
+This script is created to collect all the patients metadata, and the EEG features before start the preprocessing phase 
+what this will do is help us understand the whole picture of the patients and create a tabular resume
+
+WHAT WE HAVE AT THE END:
+
+1. a table with all the patients and their clinical features
+2. technical information about each patient
+3. Classes balance (good vs poor)
+4. CSV file that we will use as reference for the WHOLE project
+
+"""
+
+import os
+import sys 
+import wfdb
+import numpy as np
+import pandas as pd
+
