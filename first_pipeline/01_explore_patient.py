ff142e5449c32875622f5c6f13d61744df662347
mfpachecog <manuelfpg012@gmail.com>
2026-07-03 11:49:21 -0500

    feat:added torch package to project, and fixed absoulute route on 01 script

--- first_pipeline/01_explore_patient.py
+++ first_pipeline/01_explore_patient.py
@@ -17,7 +17,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #route to the file where the patients data is downloaded. 
-DATA_DIR = "/home/singular1ty/Documents/_PROJECTS/eeg-ml-project/patients_data_raw/physionet.org/files/i-care/2.1/training"
+DATA_DIR = "/home/singular1ty/Documents/_PROJECTS/ongoing_projects/eeg-ml-project/patients_data_raw/physionet.org/files/i-care/2.1/training"
 
 #patient ID from the downloaded patients
 PATIENT_ID = "0418"
